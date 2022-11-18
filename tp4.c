6481a732a3f6de26a019a89e71f3e98965ed4c69
NathanAvril <nathan.avril04@gmail.com>
2022-11-18 09:33:01 +0100

    rechercher_patient

--- tp4.c
+++ tp4.c
@@ -1,1 +1,13 @@
-
+Patient* rechercher_patient(Parbre* abr, char* mn) {
+    while (abr != NULL) {
+        int rel = strcmp(abr->nom, nm); //comparaison du nom du patient et du noeud
+        if (rel == 0) {
+            return abr; //patient trouve
+        } else if (rel > 0) {
+            abr = abr->fils_gauche; //nom du patient inferieur au noeud actuel
+        } else {
+            abr = abr->fils_droit; //nom du patient superieur au noeud actuel
+        }
+    }
+    return NULL; //le patient n'a pas ete trouve
+}
