cf5bca7e33f1ee7398869706c115b0e99ebe5e1d
NathanAvril <nathan.avril04@gmail.com>
2022-11-18 09:12:18 +0100

    Declaration des fonctions

--- tp4.h
+++ tp4.h
@@ -10,3 +10,13 @@ struct Patient {
 }
 
 typedef Patient* Parbre;
+
+Patient* CreerPatient(char* nm, char* pr);
+void inserer_patient(Parbre* abr, char* nm, char* pr);
+Patient* rechercher_patient(Parbre* abr, char* mn);
+void afficher_fiche(Parbre* abr, char* nm);
+void afficher_patients(Parbre* abr);
+Consultation* CreerConsult(char* date, char* motif, int nivu);
+void ajouter_consultation(Parbre* abr, char* nm, char* date, char* motif, int nivu);
+void supprimer_patient(Parbre* abr, char* nm);
+void maj(Parbre* abr, Parbre* abr2);
