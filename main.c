68ca2e9681c704af91c061fa4b595d90d41e997b
Raph <94909118+Ryustiel@users.noreply.github.com>
2022-12-01 23:39:11 +0100

    ChangeS...

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 // https://learn.microsoft.com/en-us/visualstudio/debugger/finding-memory-leaks-using-the-crt-library?view=vs-2022
 #include <stdio.h>
+#include <tp4.h>
 #include <tp4.c>
-
-#define INSR = 5;
 
 int main(){
     
@@ -11,10 +10,10 @@ int main(){
     Patient* root = NULL;
     Parbre liste_patients = root;
 
-    char* prenoms[INSR] = {}
-    char* noms[INSR] 
+    char* prenoms[5] = {"maggie", "ben", "charley", "iggy", "jeb"};
+    char* noms[5] = {"JOW", "SITTHIE", "BJORN", "AZAZE", "GEG"};
 
-    for (int i=0; i<INSR; i++) {
+    for (int i=0; i<5; i++) {
         inserer_patient(liste_patients, );
     }
 
