3a873e699d916249f36709448b8fd42b78bfed29
NathanAvril <nathan.avril04@gmail.com>
2022-12-02 01:56:05 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,13 +11,33 @@ void main(){
 //version test
 void test(){
     Parbre liste_patients = NULL;
+    Parbre liste2 = NULL;
 
+    //Insertion et affichage
     char* noms[15] = {"H", "D", "B", "A", "C", "F", "E", "G", "L", "N", "O", "M", "J", "K", "I"};
-
     printf("yo");
     for (int i=0; i<15; i++) {
         inserer_patient(&liste_patients, noms[i], "Luc");
     }
-
     afficher_patients(&liste_patients);
+
+    //Recherche, ajouter consultation et fiche
+    ajouter_consultation(&liste_patients, "F", "01/01/2000", "fievre", 0);
+    ajouter_consultation(&liste_patients, "K", "01/01/1999", "fievre", 0);
+    ajouter_consultation(&liste_patients, "K", "01/01/2002", "douleur", 1);
+    ajouter_consultation(&liste_patients, "K", "01/01/2001", "gastro", 2);
+    ajouter_consultation(&liste_patients, "Z", "01/01/2000", "fievre", 0);
+    ajouter_consultation(&liste2, "K", "01/01/2000", "fievre", 0);
+
+    afficher_fiche(&liste_patients, "H");
+    afficher_fiche(&liste_patients, "B");
+    afficher_fiche(&liste_patients, "F");
+    afficher_fiche(&liste_patients, "K");
+    afficher_fiche(&liste_patients, "M");
+    afficher_fiche(&liste_patients, "GO");
+    afficher_fiche(&liste_patients, "EO");
+    afficher_fiche(&liste_patients, "GA");
+    afficher_fiche(&liste_patients, "EA");
+    afficher_fiche(&liste_patients, "Z");
+    afficher_fiche(&liste2, "H");
 }
