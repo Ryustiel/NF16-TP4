e03fd11cebb217c723ff27b18be5acb225a5edc1
NathanAvril <nathan.avril04@gmail.com>
2022-12-05 21:37:47 +0100

    Debug suppression

--- tp4.c
+++ tp4.c
@@ -164,10 +164,10 @@ void ajouter_consultation(Parbre* abr, char* nm, char* date, char* motif, int ni
             ptr_prec->suivant = CreerConsult(date, motif, nivu);
             ptr_prec->suivant->suivant = ptr; // chainage simple, pas besoin de definir un precedent
 
-            }
         }
     }
 }
+
 
 
 Patient* rechercher_patient(Parbre* abr, char* nm) {
@@ -288,6 +288,7 @@ void supprimer_patient(Parbre* abr, char* nm) {
             strcpy(ptr->nom, succ->nom);
             strcpy(ptr->prenom, succ->prenom);
             ptr->ListeConsult = succ->ListeConsult;
+            succ->ListeConsult = NULL;
             ptr->nbrconsult = succ->nbrconsult;
 
             ptr = succ; // ptr pointe toujours vers le noeud a detruire en fin de boucle
@@ -383,7 +384,7 @@ void interface() {
 
     int continuer = 1, reponse, urgence; // variables utilisees pour stocker les informations saisies
     while (continuer) {
-        
+
         //affichage du menu et attente d'une reponse
         printf("Que voulez-vous faire ?\n1. Ajouter un patient\n2. Ajouter une consultation a un patient\n3. Afficher une fiche medicale\n4. Afficher la liste des patients\n5. Supprimer un patient\n6. Copier la liste des patients depuis la derniere sauvegarde\n7. Mettre a jour la sauvegarde de la liste des patients\n8. Quitter\n\n");
         scanf("%d", &reponse);
